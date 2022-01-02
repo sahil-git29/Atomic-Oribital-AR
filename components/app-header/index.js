21d1a2006fb786b845e8f76bcdc051c67a61b92d
Sahil Gulati <39591887+sahil-git29@users.noreply.github.com>
2022-01-02 14:01:59 +0530

    Atomic Orbitals and VSEPR in AR

--- components/app-header/index.js
+++ components/app-header/index.js
@@ -10,20 +10,16 @@ headerTemplate.innerHTML = /* html */ `
     padding: 1rem 4rem;
     background-color: #FFFFFF;
   }
-
   a {
     text-decoration: none;
   }
-
   figure {
     margin: 0;
   }
-
   .row {
     display: flex;
     flex-direction: row;
   }
-
   .column {
     display: flex;
     flex-direction: column;
@@ -34,7 +30,6 @@ headerTemplate.innerHTML = /* html */ `
     align-items: center;
     justify-content: space-between;
   }
-
   .menu-link {
     font-family: "Roboto", sans-serif;
     font-weight: 500;
@@ -44,17 +39,14 @@ headerTemplate.innerHTML = /* html */ `
     transition: 0.2s;
     cursor: pointer;
   }
-
   .menu-link:hover,
   .language:hover .menu-link,
   .mobile-menu-link:hover {
     color: var(--secondary);
   }
-
   .language {
     position: relative;
   }
-
   .language-menu {
     background-color: white;
     color: var(--primary);
@@ -73,20 +65,16 @@ headerTemplate.innerHTML = /* html */ `
     z-index: 1;
     box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
   }
-
   .language-menu li {
     margin: 0.5rem 0.2rem;
     transition: color 0.2s;
   }
-
   .language-menu li:hover {
     color: var(--secondary);
   }
-
   .language:hover .language-menu {
     visibility: visible;
   }
-
   .hamburger-menu {
     display: none;
     width: 25px;
@@ -97,12 +85,10 @@ headerTemplate.innerHTML = /* html */ `
     z-index: 2;
     transition: all 0.5s;
   }
-
   .hamburger-menu.active {
     position: fixed;
     right: 2rem;
   }
-
   .mobile-menu {
     display: none;
     height: 100vh;
@@ -120,21 +106,17 @@ headerTemplate.innerHTML = /* html */ `
     justify-content: center;
     background: linear-gradient(156.2deg, #173A7B 0%, #5C45EB 101.87%);
   }
-
   .mobile-menu.active {
     transform: translateX(25vw);
   }
-
   .mobile-menu-content {
     flex: 1 0 auto;
     align-items: center;
     position: relative;
   }
-
   .mobile-menu-content figure {
     margin-top: 10vh;
   }
-
   .mobile-menu-links {
     position: absolute;
     left: 50%;
@@ -142,7 +124,6 @@ headerTemplate.innerHTML = /* html */ `
     transform: translate(-50%, -50%);
     width: 12rem;
   }
-
   .mobile-menu-link {
     color: #FFFFFF;
     text-align: center;
@@ -153,7 +134,6 @@ headerTemplate.innerHTML = /* html */ `
     will-change: color;
     transition: 0.2s;
   }
-
   .mobile-menu-footer {
     flex-shrink: 0;
     font-family: "Roboto", sans-serif;
@@ -161,7 +141,6 @@ headerTemplate.innerHTML = /* html */ `
     color: #FFFFFF;
     text-align: center;
   }
-
   .overlay {
     background: rgba(0, 0, 0, 0.5);
     position: fixed;
@@ -173,13 +152,11 @@ headerTemplate.innerHTML = /* html */ `
     visibility: hidden;
     transition: 0.5s;
   }
-
   .overlay.active {
     opacity: 1;
     visibility: visible;
     z-index: 1;
   }
-
   .bar {
     width: 25px;
     height: 3px;
@@ -188,11 +165,9 @@ headerTemplate.innerHTML = /* html */ `
     border-radius: 2px;
     transition: 0.5s;
   }
-
   .hamburger-menu.active .bar {
     background-color: #FFFFFF;
   }
-
   @keyframes bar1in {
     0% {
       transform: translateY(0);
@@ -204,7 +179,6 @@ headerTemplate.innerHTML = /* html */ `
       transform: translateY(7px) rotate(45deg);
     }
   }
-
   @keyframes bar2in {
     0% {
       opacity: 1;
@@ -216,7 +190,6 @@ headerTemplate.innerHTML = /* html */ `
       opacity: 0;
     }
   }
-
   @keyframes bar3in {
     0% {
       transform: translateY(0);
@@ -228,7 +201,6 @@ headerTemplate.innerHTML = /* html */ `
       transform: translateY(-7px) rotate(-45deg);
     }
   }
-
   @keyframes bar1out {
     0% {
       transform: translateY(7px) rotate(45deg);
@@ -240,7 +212,6 @@ headerTemplate.innerHTML = /* html */ `
       transform: translateY(0);
     }
   }
-
   @keyframes bar2out {
     0% {
       opacity: 0;
@@ -252,7 +223,6 @@ headerTemplate.innerHTML = /* html */ `
       opacity: 1;
     }
   }
-
   @keyframes bar3out {
     0% {
       transform: translateY(-7px) rotate(-45deg);
@@ -264,7 +234,6 @@ headerTemplate.innerHTML = /* html */ `
       transform: translateY(0);
     }
   }
-
   @media screen and (max-width: 800px) {
     .hamburger-menu {
       display: flex;
@@ -287,109 +256,37 @@ headerTemplate.innerHTML = /* html */ `
   </style>
   <div id="overlay" class="overlay"></div>
   <header class="row">
-    <figure>
-      <a href="/index.html">
-        <molecule-icon></molecule-icon>
-      </a>
-    </figure>
-    <div class="menu row">
-      <a class="menu-link" href="/pages/about.html">
-        <intl-message key="menu.about"></intl-message>
-      </a>
-
-      <a class="menu-link" href="/pages/contact.html">
-        <intl-message key="menu.getInTouch"></intl-message>
-      </a>
-
-      <a
-        href="/pages/feedback.html"
-        class="menu-link"
-      >
-        <intl-message key="menu.feedback"></intl-message>
-      </a>
-
-      <a
-        href="/assets/markers/allmarkers.pdf"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="menu-link"
-      >
-        <intl-message key="menu.markers"></intl-message>
-      </a>
-
+    
+  
+     
+    
       <a
         class="menu-link"
         id="instructions"
       >
-        <intl-message key="menu.instructions"></intl-message>
+        <intl-message key="Home"></intl-message>
       </a>
-
-      <div class="language">
-        <a class="menu-link language">
-          <intl-message key="menu.language"></intl-message>
-        </a>
-
-        <ul class="language-menu">
-          <li id="en">English</li>
-          <li id="fr">Français</li>
-          <li id="de">Deutsche</li>
-          <li id="it">Italiano</li>
-          <li id="es">Español</li>
-          <li id="pt">Português</li>
-        </ul>
-      </div>
+      
     </div>
-
     <div id="hamburger" class="hamburger-menu">
       <div id="bar1" class="bar"></div>
       <div id="bar2" class="bar"></div>
       <div id="bar3" class="bar"></div>
     </div>
   </header>
-
   <div id="mobile-menu" class="mobile-menu column">
     <div class="mobile-menu-content column">
-      <figure>
-        <a href="/index.html">
-          <molecule-icon medium no-strokes />
-        </a>
-      </figure>
+      
       <div class="mobile-menu-links column">
-        <a class="mobile-menu-link"
-           href="/pages/about.html"
-        >
-          <intl-message key="menu.about"></intl-message>
-        </a>
-
-        <a class="mobile-menu-link"
-           href="/pages/contact.html"
-        >
-          <intl-message key="menu.getInTouch"></intl-message>
-        </a>
-
-        <a
-          class="mobile-menu-link"
-          href="/pages/feedback.html"
-        >
-          <intl-message key="menu.feedback"></intl-message>
-        </a>
-
-        <a
-          href="/assets/markers/allmarkers.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="mobile-menu-link"
-        >
-          <intl-message key="menu.markers"></intl-message>
-        </a>
-
+        
+        
+        
         <a
           id="instructions-mobile"
           class="mobile-menu-link"
         >
-          <intl-message key="menu.instructions"></intl-message>
+          <intl-message key="Home"></intl-message>
         </a>
-
         <a class="mobile-menu-link" id="lang-option">
           <intl-message key="menu.language"></intl-message>
         </a>
@@ -412,23 +309,18 @@ headerTemplate.innerHTML = /* html */ `
         <a class="mobile-menu-link" id="en_">
           English
         </a>
-
         <a class="mobile-menu-link" id="es_">
           Español
         </a>
-
         <a class="mobile-menu-link" id="fr_">
           Français
         </a>
-
         <a class="mobile-menu-link" id="it_">
           Italiano
         </a>
-
         <a class="mobile-menu-link" id="pt_">
           Português
         </a>
-
         <a class="mobile-menu-link" id="de_">
           Deutsche
         </a>
@@ -439,7 +331,6 @@ headerTemplate.innerHTML = /* html */ `
       <intl-message key="app.policy"></intl-message>
     </p>
   </div>
-
   `;
 
 class AppHeader extends HTMLElement {
--- modules/utils/handleOverlays.js
+++ modules/utils/handleOverlays.js
@@ -1,55 +1,55 @@
-var instructionsOverlay = document.getElementById("instructions");
-var instructionsButton = document.querySelector("toggle-instructions");
+// var instructionsOverlay = document.getElementById("instructions");
+// var instructionsButton = document.querySelector("toggle-instructions");
 
-var menuOverlay = document.getElementById("menu");
-var menuButton = document.querySelector("toggle-menu");
+// var menuOverlay = document.getElementById("menu");
+// var menuButton = document.querySelector("toggle-menu");
 
-var descriptionOverlay = document.getElementById("description");
-var descriptionButton = document.querySelector("toggle-description");
+// var descriptionOverlay = document.getElementById("description");
+// var descriptionButton = document.querySelector("toggle-description");
 
 var zoomMenu = document.querySelector("zoom-icon");
 var zoomMenuContainer = document.getElementById("zoom-container");
 
-var camMenu = document.querySelector("camera-icon");
-var camMenuContainer = document.getElementById("cam-container");
+// var camMenu = document.querySelector("camera-icon");
+// var camMenuContainer = document.getElementById("cam-container");
 
-function hideInstructionsOverlay() {
-  instructionsOverlay.toggle();
-}
+// function hideInstructionsOverlay() {
+//   instructionsOverlay.toggle();
+// }
 
-function hideDescriptionOverlay() {
-  descriptionOverlay.toggle();
-}
+// function hideDescriptionOverlay() {
+//   descriptionOverlay.toggle();
+// }
 
-function hideMenuOverlay() {
-  menuOverlay.toggle();
-}
+// function hideMenuOverlay() {
+//   menuOverlay.toggle();
+// }
 
 function handleZoomMenu(e) {
   zoomMenuContainer.classList.toggle("hide");
   zoomMenu.isActive = !zoomMenu.isActive;
 
-  if (camMenu.isActive) {
-    camMenu.isActive = false;
-    camMenuContainer.classList.add("hide");
-  }
+  // if (camMenu.isActive) {
+  //   camMenu.isActive = false;
+  //   camMenuContainer.classList.add("hide");
+  // }
 }
 
-function handleCamMenu(e) {
-  camMenuContainer.classList.toggle("hide");
-  camMenu.isActive = !camMenu.isActive;
+// function handleCamMenu(e) {
+//   camMenuContainer.classList.toggle("hide");
+//   camMenu.isActive = !camMenu.isActive;
 
-  if (zoomMenu.isActive) {
-    zoomMenu.isActive = false;
-    zoomMenuContainer.classList.add("hide");
-  }
-}
+//   if (zoomMenu.isActive) {
+//     zoomMenu.isActive = false;
+//     zoomMenuContainer.classList.add("hide");
+//   }
+// }
 
-instructionsButton.addEventListener("toggleInstructions", hideInstructionsOverlay);
-descriptionButton.addEventListener("toggleDescription", hideDescriptionOverlay);
-menuButton.addEventListener("toggleMenu", hideMenuOverlay);
+// instructionsButton.addEventListener("toggleInstructions", hideInstructionsOverlay);
+// descriptionButton.addEventListener("toggleDescription", hideDescriptionOverlay);
+// menuButton.addEventListener("toggleMenu", hideMenuOverlay);
 
 if(window.location.pathname !== "/modules/virtual-modeling-kit/") {
   zoomMenu.addEventListener("click", handleZoomMenu);
-  camMenu.addEventListener("click", handleCamMenu);
+  // camMenu.addEventListener("click", handleCamMenu);
 }
